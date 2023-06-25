a155f9322ed860e829060dc172c403f94b9bb929
Swagat47 <swagatraj47@gmail.com>
2023-06-25 12:24:39 +0530

    extends me eslint added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["@react-native-community", "prettier", "plugin:react/recommended", "airbnb"],
+  extends: ["eslint:recommended", "@react-native-community", "prettier", "plugin:react/recommended", "airbnb"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
